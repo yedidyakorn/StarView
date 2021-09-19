583344a974d607ebd901def4cdc3400aaac6844a
ידידיה קורן <ykorn@g.jct.ac.il>
2021-09-19 13:10:48 +0300

    apod api

--- StarView/MainWindow.xaml.cs
+++ StarView/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Apod;
 
 namespace StarView
 {
@@ -23,6 +24,9 @@ namespace StarView
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+        
     }
 }
